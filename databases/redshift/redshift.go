5d1c208a04eef6315e357db4826e88501c7a2ebd
adarsh-jaiss <its.adarshjaiss@gmail.com>
2024-05-18 00:02:10 +0530

    updated redshift
    Signed-off-by: adarsh-jaiss <its.adarshjaiss@gmail.com>

--- databases/redshift/redshift.go
+++ databases/redshift/redshift.go
@@ -5,14 +5,19 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"os"
 	"strings"
+
+	_ "github.com/lib/pq"
 	"github.com/thesaas-company/xray/config"
 	"github.com/thesaas-company/xray/types"
 )
 
+var DB_PASSWORD = "DB_PASSWORD"
+
 const (
-	Redshift_Schema_query = "SHOW COLUMNS FROM TABLE %s.%s.%s;"
-	Redshift_Tables_query = "SELECT * FROM svv_all_tables WHERE database_name = '%s';"
+	Redshift_Schema_query = `SELECT "column", type, encoding, distkey, sortkey, "notnull"  FROM pg_table_def WHERE schemaname = '%s' AND tablename = '%s';`
+	Redshift_Tables_query = "SHOW TABLES FROM SCHEMA %s.public;"
 )
 
 type Redshift struct {
@@ -28,9 +33,13 @@ func NewRedshift(client *sql.DB) (types.ISQL, error) {
 }
 
 func NewRedshiftWithConfig(cfg *config.Config) (types.ISQL, error) {
+	if os.Getenv(DB_PASSWORD) == "" || len(os.Getenv(DB_PASSWORD)) == 0 {
+		return nil, fmt.Errorf("please set %s env variable for the database", DB_PASSWORD)
+	}
+	DB_PASSWORD = os.Getenv(DB_PASSWORD)
 
-	dsn := fmt.Sprintf("arn:aws:secretsmanager:%s:%s:secret:%s", cfg.Region, cfg.AccountID, cfg.SecretName)
-	db, err := sql.Open("redshift-data", dsn)
+	dsn := fmt.Sprintf("host=%s port=%v user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.Username, DB_PASSWORD, cfg.DatabaseName, cfg.SSL)
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("error creating a new session : %v", err)
 	}
@@ -42,8 +51,11 @@ func NewRedshiftWithConfig(cfg *config.Config) (types.ISQL, error) {
 }
 
 func (r *Redshift) Schema(table string) (types.Table, error) {
+	if len(r.Config.Schema) == 0 {
+		r.Config.Schema = "public"
+	}
 
-	query := fmt.Sprintf(Redshift_Schema_query, r.Config.DatabaseName, r.Config.Schema, table)
+	query := fmt.Sprintf(Redshift_Schema_query, r.Config.Schema, table)
 	ctx := context.Background()
 	rows, err := r.Client.QueryContext(ctx, query)
 	if err != nil {
@@ -53,23 +65,22 @@ func (r *Redshift) Schema(table string) (types.Table, error) {
 	var columns []types.Column
 	for rows.Next() {
 		var column types.Column
+		var encoding string
+		var distkey bool
+		var sortkey int
+		var notnull bool
 		if err := rows.Scan(
 			&column.Name,
 			&column.Type,
-			&column.IsNullable,
-			&column.DefaultValue,
-			&column.CharacterMaximumLength,
-			&column.OrdinalPosition,
-			&column.Visibility,
-			&column.IsPrimary,
-			&column.IsUpdatable,
+			&encoding,
+			&distkey,
+			&sortkey,
+			&notnull,
 		); err != nil {
 			return types.Table{}, fmt.Errorf("error scanning rows: %v", err)
 		}
-		column.Metatags = []string{}
-		column.Metatags = append(column.Metatags, column.Name)
+		column.Metatags = []string{encoding, fmt.Sprintf("distkey:%v", distkey), fmt.Sprintf("sortkey:%d", sortkey), fmt.Sprintf("notnull:%v", notnull)}
 		columns = append(columns, column)
-
 	}
 
 	if err := rows.Err(); err != nil {
@@ -85,23 +96,35 @@ func (r *Redshift) Schema(table string) (types.Table, error) {
 	}, nil
 }
 
+type TableResponse struct {
+	TableName    string
+	DatabaseName string
+	SchemaName   string
+	TableType    string
+	TableAcl     sql.NullString
+	Remarks      sql.NullString
+}
+
 func (r *Redshift) Tables(databaseName string) ([]string, error) {
-	ctx := context.Background()
+	// ctx := context.Background()
 	query := fmt.Sprintf(Redshift_Tables_query, databaseName)
 
-	res, err := r.Client.QueryContext(ctx, query)
+	res, err := r.Client.Query(query)
 	if err != nil {
 		return nil, fmt.Errorf("error executing query: %v", err)
 	}
 
 	var tables []string
+
 	for res.Next() {
-		var table string
-		if err := res.Scan(&table); err != nil {
+		var table TableResponse
+		if err := res.Scan(&table.DatabaseName, &table.SchemaName, &table.TableName, &table.TableType, &table.TableAcl, &table.Remarks); err != nil {
 			return nil, fmt.Errorf("error scanning result: %v", err)
 		}
-		tables = append(tables, table)
+		fmt.Println(table)
+		tables = append(tables, table.TableName)
 	}
+	fmt.Println(tables)
 
 	if err := res.Err(); err != nil {
 		return nil, fmt.Errorf("error iterating over result: %v", err)
